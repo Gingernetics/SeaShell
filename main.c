188751af9cd1b70d2921761ec087bf031abd4af3
Gingernetics <janstefan.kowalski@gmail.com>
2017-11-28 11:01:46 -0500

    Corrected Input by using fgets

--- main.c
+++ main.c
@@ -13,42 +13,42 @@ char ** parse_args( char * line ){
 	//char * string is only used to test the size of strings
 	char * string;
 
-	char ** arguments = calloc(20, 8);
+	char ** arguments = calloc(ARGLIMIT, 8);
 	char * element = malloc(8);
 
 	int i = 0;
-	/*
-	printf("%s\n", strsep(&line, " "));
-	printf("%s\n", strsep(&line, " "));
-*/
-/*
+
 	while((element = strsep(&line, " ")) != NULL){
 		arguments[i++] = element;
 		printf("%s\n", element);
 	}
-*/
-	//arguments[i] = NULL;
+
+	arguments[i] = NULL;
 	return arguments;
 }
 
 int main(){
 
 	char line[100];
-	scanf("%s", line);
-	printf("%s\n", line);
+	fgets(line, sizeof(line), stdin);
+	//Strsep by ;, then while loop
 	char **result = parse_args(line);
 
 	int parent = fork();
-
+/*
 	if(parent){
 		int status;
 		wait(&status);
+		printf("PARENT\n");
 	}
 
 	if(!parent){
+		printf("CHILD\n");
+		printf("%s\n", result[0]);
 		//execvp automatically releases memory
+*/
 		execvp(result[0], result);
-	}
+//	}
 
 	return 0;
 }
